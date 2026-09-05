3b35a70e0b4acc6d2f56d1d605eeccb4a82b125d
Anturia Hassani <anturia.hm@gmail.com>
2026-09-05 04:43:37 +0200

    debug with counting variables

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,33 +5,37 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	
-	private static int rashCount = 0;		
-	private static int pupilCount = 0;		
+	private static int headCount;	
+	private static int rashCount;		
+	private static int pupilCount;		
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader (new FileReader("../symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	
-		int headCount = 0;	
-		while (line != null) {
-			i++;	
+		headCount = 0;	
+		rashCount = 0;
+		pupilCount = 0;
+		while (line != null) {	
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headCount++;
 				System.out.println("number of headaches: " + headCount);
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
+				System.out.println("number of rashes: " + rashCount);
 			}
 			else if (line.contains("pupils")) {
 				pupilCount++;
+				System.out.println("number of pupils: " + pupilCount);
 			}
 
 			line = reader.readLine();	// get another symptom
 		}
+
+		reader.close();
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
