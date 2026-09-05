6cf307a2576a73d61409147ca851fb2100478aa5
Anturia Hassani <anturia.hm@gmail.com>
2026-09-05 05:30:19 +0200

    writer implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -0,0 +1,33 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+    private String filepath;
+
+    public WriteSymptomDataToFile(String filepath) {
+        this.filepath = filepath;
+    }
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filepath));
+
+            for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue());
+                writer.newLine();
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
